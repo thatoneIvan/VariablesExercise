a0714fe3c66a148e8a53e43b4911bc4702c5a1d7
“thatoneIvan” <ivanh.sanchez84@gmail.com>
2022-03-08 09:40:50 -0600

    updated variable excercise

---
+++ script.js
@@ -0,0 +1,45 @@
+//Variables
+//1
+//const
+
+//2
+let num = 20;
+// 3
+num = 15;
+
+// 4
+num = num +7;
+
+// 5
+num--;
+
+// 6
+num = "This is not a number";
+
+// 7
+let blank
+
+// 8
+console.log(blank);
+
+// 9
+blank = null;
+
+// BONUS
+// 1
+const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+
+// 2
+console.log(quarter, dime, nickel ,penny );
+
+// 3
+let letters = "qwertyuiop";
+
+// 4
+console.log(letters.length);//p
+
+// other options
+//console.log(letters.slice(9));//penny
+console.log(letters[letters.length -1]);
+
+
